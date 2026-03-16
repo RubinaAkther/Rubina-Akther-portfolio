92f749b10ccc882c28be1a41b1f273cc2d074614
Rubina Akther <rubinaakther3454@gmail.com>
2026-03-16 08:50:03 +0600

    link GitHub and LinkedIn profiles in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,11 @@
-import { FaArrowRight } from "react-icons/fa";
-import MagicButton from "./ui/MagicButton";
-import { socialMedia } from "@/data";
-import { div } from "motion/react-client";
+import { socialMedia } from '@/data';
+import { FaArrowRight } from 'react-icons/fa';
+import MagicButton from './ui/MagicButton';
 
 /* eslint-disable @next/next/no-img-element */
 const Footer = () => {
   return (
     <footer className="w-full pb-10 " id="contact">
-      
       <div className="flex flex-col items-center">
         <h1 className=" lg:max-w-[45vw] heading text-4xl font-bold text-center pb-15">
           Ready to take <span className="text-purple-300">your</span> digital
@@ -18,16 +16,34 @@ const Footer = () => {
           achieve your goals.
         </p>
         <a href="mailto:rubinaakther3454@gmail.com" target="_blank">
-          <MagicButton title="Let's get in touch" icon= {<FaArrowRight/>} position="right"/>
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaArrowRight />}
+            position="right"
+          />
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light ">Copyright &copy; 2026 Rubina Akther </p>
+        <p className="md:text-base text-sm md:font-normal font-light ">
+          Copyright &copy; 2026 Rubina Akther{' '}
+        </p>
         <div className="flex items-center md:gap-3 gap-6 mt-10">
           {socialMedia.map((profile) => (
-            <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-              <img src={profile.img} alt={profile.img}  width={20} height={20}/>
-            </div>
+            <a
+              key={profile.id}
+              href={profile.link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 hover:bg-zinc-800 transition-all">
+                <img
+                  src={profile.img}
+                  alt={`social-${profile.id}`}
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </a>
           ))}
         </div>
       </div>
